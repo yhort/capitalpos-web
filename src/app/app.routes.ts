c594e2abb4b2759143d3b0ad17ec3a6ec3cfbbae
Yhort Cruz <yhortcruz.a@gmail.com>
2026-07-02 12:57:56 -0500

    Configurar rutas y dashboard inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ShellComponent } from './layout/shell/shell.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'app/dashboard',
+    },
+    {
+        path: 'app',
+        component: ShellComponent,
+        children: [
+            {
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'dashboard',
+            },
+            {
+                path: 'dashboard',
+                title: 'Dashboard | CapitalPOS',
+                loadComponent: () =>
+                    import(
+                        './features/dashboard/pages/dashboard-page/dashboard-page.component'
+                    ).then((component) => component.DashboardPageComponent),
+            },
+        ],
+    },
+    {
+        path: '**',
+        redirectTo: 'app/dashboard',
+    },
+];
---
+++ src/app/features/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [],
+  templateUrl: './dashboard-page.component.html',
+  styleUrl: './dashboard-page.component.scss',
+})
+export class DashboardPageComponent {}
